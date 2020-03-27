befcd0ad833868d1533324bb3c18a909904dc124
Odium-Mundi <61995344+Odium-Mundi@users.noreply.github.com>
2020-03-27 15:25:57 +0300

    Create Mile.java

---
+++ Mile.java
@@ -0,0 +1,8 @@
+public class Mile {
+    public static void main(String[]args) {
+        int price_ticket, bonus;
+        price_ticket = 1000;
+        bonus = price_ticket / 20;
+        System.out.println("Количество миль: " + bonus);
+    }
+}
